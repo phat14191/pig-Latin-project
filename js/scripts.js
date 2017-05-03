b2be85ca1ed5e254998d89698ac55a00f1f9d269
Guy Anderson, Jun Fritz and Jordan Loop <guy@>
2017-05-03 11:19:51 -0700

    add function to check if one character words contain i or a.

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,19 @@ var isNumber = function(userArray){
 var isFirstVowel = function(userArray) {
   for (var i = 0; i < vowels.length; i++){
     if (userArray[0] === vowels[i]){
-      return "vowel found";
+      return true;
     }
+  }
+}
+
+var isOneWordVowel = function(wordArray) {
+
+  for (var i = 0; i < wordArray.length; i++){
+    if ((wordArray[i].length === 1) && (wordArray[i] === "i") || (wordArray[i] === "a")){
+    wordArray[i] += "ay";
+    var answer = wordArray.join(" ");
+    return answer;
+    } 
   }
 }
 
@@ -25,16 +36,38 @@ $(document).ready(function(){
   event.preventDefault();
   var userInput = $("input#sentence").val();
   var userArray = userInput.split("");
+  var wordArray = userInput.split(" ");
 
   var numberTest = isNumber(userArray);
   var vowelTest = isFirstVowel(userArray);
+  var singleTest = isOneWordVowel(wordArray);
 
   if (numberTest === false){
     $(".output").append("<p>" + userInput + "</p>");
-  } else {
-    $(".output").append("<p>" + vowelTest + "</p>");
   }
-  
+  else{
+
+
+    // var beginningVowel = wordArray.join("");
+    $(".output").append("<p>" + singleTest + "</p>");
+  }
+
+
   });
 });
-// console.log(userArray[i]);
+
+
+// WORKING single letter word that is vowel CODE
+// else if (vowelTest === true){
+//   userArray.push("ay")
+//   var beginningVowel = userArray.join("");
+//   $(".output").append("<p>" + beginningVowel + "</p>");
+
+// final for-loop
+// for(var i = 0; i < wordArray; i++){
+//   if (wordArray[i] is a  single letter word){
+//     call isOneWordVowel
+//   }else if (multiple letter word) {
+//     call isMu
+//   }
+// }
