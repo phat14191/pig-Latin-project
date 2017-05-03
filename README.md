cb712f543ec8b590efb077b631b9b9eda7f1f12f
Guy Anderson, Jun Fritz and Jordan Loop <jun@>
2017-05-03 10:11:51 -0700

    add function to check first index of userArray for vowel.

---
+++ README.md
@@ -0,0 +1,3 @@
+The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels
+input:3
+output:3
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
 
 var isNumber = function(userArray){
   for (var i = 0; i < userArray.length; i++) {
@@ -9,19 +10,31 @@ var isNumber = function(userArray){
     }
   }
 }
+
+var isFirstVowel = function(userArray) {
+  for (var i = 0; i < vowels.length; i++){
+    if (userArray[0] === vowels[i]){
+      return "vowel found";
+    }
+  }
+}
+
+
 $(document).ready(function(){
   $("form#pig").submit(function(event) {
   event.preventDefault();
   var userInput = $("input#sentence").val();
   var userArray = userInput.split("");
 
+  var numberTest = isNumber(userArray);
+  var vowelTest = isFirstVowel(userArray);
 
-  var test = isNumber(userArray);
-  if (test === false){
-      console.log("not a string");
+  if (numberTest === false){
+    $(".output").append("<p>" + userInput + "</p>");
   } else {
-    
+    $(".output").append("<p>" + vowelTest + "</p>");
   }
+  
   });
 });
 // console.log(userArray[i]);
